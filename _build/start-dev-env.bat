922df2e5a5301e33bc4bffd8b232361262f52078
Douglas Cuthbertson <doug.cuthbertson@gmail.com>
2016-02-02 17:19:27 -0500

    Replaced redcarpet markdown with kramdown, and removed the python2 activation from my local start script.

--- _build/start-dev-env.bat
+++ _build/start-dev-env.bat
@@ -1,9 +1,6 @@
 @echo off
 call bundle update
 
-REM Use the Anaconda Python 2 environment for Jekyll
-call activate py2
-
 REM Start Compass and Jekyll
 start bundle exec compass watch
 start bundle exec jekyll serve --drafts
